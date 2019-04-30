fae05359b74cc243b718c0a51039e9601bb5ee74
white <white@WHITE1016.academia.loc>
2019-04-30 13:00:12 -0500

    Second commit

--- src/main/java/com/dekses/jersey/docker/demo/Main.java
+++ src/main/java/com/dekses/jersey/docker/demo/Main.java
@@ -6,13 +6,6 @@ import org.glassfish.jersey.server.ResourceConfig;
 
 import java.io.IOException;
 import java.net.URI;
-import java.util.Scanner;
-import javax.ws.rs.core.MediaType;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.WebResource;
 
 /**
  * Main class.
@@ -42,115 +35,12 @@ public class Main {
      * @throws IOException
      */
     public static void main(String[] args) throws IOException {
-//        final HttpServer server = startServer();
-//        System.out.println(String.format("Jersey app started with WADL available at "
-//                + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
-//        System.in.read();
-//        server.stop();
-        try {
-
-            Client client = Client.create();
-            WebResource webResource = null;
-            ClientResponse response = null;
-            int opcion = -1;
-            Scanner lectura = new Scanner(System.in);
-            System.out.println("Bienvenido sistema gestion pacientes");
-         
-            do {
-                System.out.println("1. Listar 2. Crear. 3. Borrar. 4.  Salir");
-                opcion = lectura.nextInt();
-                switch (opcion) {
-                    case 1:
-                        webResource = client.resource("http://localhost:8080/myapp/paciente");
-
-                        response = webResource.accept(MediaType.APPLICATION_JSON).get(ClientResponse.class);
-
-                        if (response.getStatus() != 200) {
-                            throw new RuntimeException("Failed : HTTP error code : "
-                                    + response.getStatus());
-                        }
-
-                        String output = response.getEntity(String.class);
-                        System.out.println("Output from Server .... \n");
-                        System.out.println(output);
-                        break;
-
-                    case 2:
-                        webResource = client
-                                .resource("http://localhost:8080/myapp/paciente");
-
-                        ObjectMapper mapper = new ObjectMapper();
-                        Paciente paciente = new Paciente();
-                        
-                        System.out.println("Ingrese el codigo");
-                        String codigo = lectura.next();
-                        paciente.setId(codigo);
-                        
-                        System.out.println("Ingrese el nombre");
-                        String nombre = lectura.next();
-                        paciente.setNombre(nombre);
-                        
-                        System.out.println("Ingrese el apellido");
-                        String apellido = lectura.next();
-                        paciente.setNombre(apellido);
-                        
-                        System.out.println("Ingrese la fecha de nacimiento");
-                        String fechaNac = lectura.next();
-                        paciente.setNombre(fechaNac);
-                        
-                        System.out.println("Ingrese el teléfono");
-                        String tel = lectura.next();
-                        paciente.setNombre(tel);
-                        
-                        System.out.println("Ingrese el número médico");
-                        String medNum = lectura.next();
-                        paciente.setNombre(medNum);
-                        
-                        System.out.println("Ingrese el estado");
-                        String estado = lectura.next();
-                        paciente.setNombre(estado);
-                        
-                        String input = mapper.writeValueAsString(paciente);
-                        //Luego se utilizara Jackson
-                        //String input = "{\"empNo\":\"E11\",\"empName\":\"" + nombre + "\",\"position\":\"Salesman\"}";
-
-                        response = webResource.type(MediaType.APPLICATION_JSON).put(ClientResponse.class, input);
-
-                        if (response.getStatus() != 200) {
-                            System.out.println(response.toString());
-                            throw new RuntimeException("Failed : HTTP error code : "
-                                    + response.getStatus());
-                        }
-
-                        output = response.getEntity(String.class);
-                        System.out.println(output);
-                        break;
-
-                    case 3:
-                        System.out.println("Indique el ID del paciente");
-                        String id = lectura.next();
-                        webResource = client.resource("http://localhost:8090/myApp/rest/employees/" + id);
-
-                        response = webResource.accept(MediaType.APPLICATION_JSON).delete(ClientResponse.class);
-
-                        if (response.getStatus() != 204) {
-                            throw new RuntimeException("Failed : HTTP error code : "
-                                    + response.getStatus());
-                        }
-
-                        break;
-                    default:
-                        System.out.println("Opcion invalida");
-
-                }
-
-            } while (opcion != 4);
-
-        } catch (Exception e) {
-
-            e.printStackTrace();
-
-        }
+        final HttpServer server = startServer();
+        System.out.println(String.format("Jersey app started with WADL available at "
+                + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
+        System.in.read();
+        server.stop();
+        
     }
 }
 
