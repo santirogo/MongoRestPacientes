686f986006d0f0c3dec02c187408350a3c30a96d
Santiago <santirogo@gmail.com>
2019-05-07 22:07:18 -0500

    First commit

--- src/main/java/com/dekses/jersey/docker/demo/PacienteDAO.java
+++ src/main/java/com/dekses/jersey/docker/demo/PacienteDAO.java
@@ -5,7 +5,16 @@
  */
 package com.dekses.jersey.docker.demo;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
@@ -16,44 +25,118 @@ import java.util.Map;
  * @author white
  */
 public class PacienteDAO {
-    private static final Map<String, Paciente> pMap = new HashMap<String, Paciente>();
  
-    static {
-        initEmps();
+    public static Paciente addPaciente(Paciente pac) {
+        try{
+            MongoCredential credential = MongoCredential.createCredential("administrator", "admin", "mypassword".toCharArray());
+            
+            MongoClient mongoClient = new MongoClient(new ServerAddress("localhost"), Arrays.asList(credential));
+            
+            DB db = mongoClient.getDB("admin");
+            
+            DBCollection coll = db.getCollection("pacientes");
+            
+            DBObject doc = new BasicDBObject("id", pac.getId()).append("nombre", pac.getNombre()).append("apellido", pac.getApellido()).append("fechaNac", pac.getFechaNac()).append("direccion", pac.getDireccion()).append("tel", pac.getTel()).append("medNum", pac.getMedNum()).append("estado", pac.getEstado());
+            
+            coll.insert(doc);
+            
+
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": "
+                    + e.getMessage());
+        }
+        return pac;
     }
  
-    private static void initEmps() {
-        Paciente p1 = new Paciente("E01", "Santiago", "Romero","29/02/1990","calle falsa 123","7276382191","871273","privado");
-        Paciente p2 = new Paciente("E02", "Andrés", "González","30/02/1990","calle falsa 234","7872347479","254245","privado");
-        Paciente p3 = new Paciente("E03", "Arya", "Stark","31/02/1990","calle falsa 123","8823838992","789456","publica");
- 
-        pMap.put(p1.getId(), p1);
-        pMap.put(p2.getId(), p2);
-        pMap.put(p3.getId(), p3);
-    }
- 
-    public static Paciente getPaciente(String pNo) {
-        return pMap.get(pNo);
-    }
- 
-    public static Paciente addPaciente(Paciente equ) {
-        pMap.put(equ.getId(), equ);
-        return equ;
-    }
- 
-    public static Paciente updatePaciente(Paciente equ) {
-        pMap.put(equ.getId(), equ);
-        return equ;
+    public static Paciente updatePaciente(Paciente pac) {
+        try {
+            
+            MongoCredential credential = MongoCredential.createCredential("administrator", "admin", "mypassword".toCharArray());
+
+            MongoClient mongoClient = new MongoClient(new ServerAddress("localhost"), Arrays.asList(credential));
+
+            DB db = mongoClient.getDB("admin");
+
+            DBCollection coll = db.getCollection("pacientes");
+            BasicDBObject newDocument = new BasicDBObject();
+            
+            newDocument.put("id",pac.getId());
+            newDocument.put("nombre",pac.getNombre());
+            newDocument.put("apellido",pac.getApellido());
+            newDocument.put("fechaNac",pac.getFechaNac());
+            newDocument.put("direccion",pac.getDireccion());
+            newDocument.put("tel",pac.getTel());
+            newDocument.put("medNum",pac.getMedNum());
+            newDocument.put("estado",pac.getEstado());
+            
+            DBObject searchQuery = new BasicDBObject().append("id", pac.getId());
+            coll.update(searchQuery, newDocument);
+
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": "
+                    + e.getMessage());
+        }
+        return pac;
     }
  
     public static void deletePaciente(String pNo) {
-        pMap.remove(pNo);
+        try {
+            System.out.println("delete!");
+            MongoCredential credential = MongoCredential.createCredential("administrator", "admin", "mypassword".toCharArray());
+
+            MongoClient mongoClient = new MongoClient(new ServerAddress("localhost"), Arrays.asList(credential));
+
+            DB db = mongoClient.getDB("admin");
+            DBCollection coll = db.getCollection("pacientes");
+            DBObject doc = new BasicDBObject();
+            doc.put("id", pNo);
+            System.out.println("doc: "+doc);
+            coll.remove(doc);
+
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": "
+                    + e.getMessage());
+        }
     }
  
     public static List<Paciente> getAllPacientes() {
-        Collection<Paciente> c = pMap.values();
         List<Paciente> list = new ArrayList<Paciente>();
-        list.addAll(c);
+        
+        try {
+            
+            MongoCredential credential = MongoCredential.createCredential("administrator","admin", "mypassword".toCharArray());
+
+            MongoClient mongoClient = new MongoClient(new ServerAddress("localhost"), Arrays.asList(credential));
+
+            DB db = mongoClient.getDB("admin");
+
+            DBCollection coll = db.getCollection("pacientes");
+
+            DBCursor cursor = coll.find();
+            try {
+                while (cursor.hasNext()) {
+                    DBObject object = cursor.next();
+                    String id = object.get("id").toString();
+                    String nombre = object.get("nombre").toString();
+                    String apellido = object.get("apellido").toString();
+                    String fechaNac = object.get("fechaNac").toString();
+                    String direccion = object.get("direccion").toString();
+                    String tel = object.get("tel").toString();
+                    String medNum = object.get("medNum").toString();
+                    String estado = object.get("estado").toString();
+                    Paciente p = new Paciente(id, nombre, apellido, fechaNac, direccion, tel, medNum, estado);
+                    list.add(p);
+                    System.out.println(object);
+                }
+            } finally {
+                cursor.close();
+            }
+
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": "
+                    + e.getMessage());
+        }
+        
         return list;
     }
      
--- src/main/java/com/dekses/jersey/docker/demo/PacienteREST.java
+++ src/main/java/com/dekses/jersey/docker/demo/PacienteREST.java
@@ -1,14 +1,17 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * To change this tpaclate file, choose Tools | Tpaclates
+ * and open the tpaclate in the editor.
  */
 package com.dekses.jersey.docker.demo;
 
 import java.util.List;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -19,7 +22,7 @@ import javax.ws.rs.core.MediaType;
 @Path("/paciente")
 public class PacienteREST {
     // URI:
-    // /contextPath/servletPath/employees
+    // /contextPath/servletPath/paciente
     @GET
     @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
     public List<Paciente> getPacientes_JSON() {
@@ -28,10 +31,25 @@ public class PacienteREST {
     }
     
     // URI:
-    // /contextPath/servletPath/employees
+    // /contextPath/servletPath/paciente
     @POST
     @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-    public Paciente addPaciente(Paciente emp) {
-        return PacienteDAO.addPaciente(emp);
+    public Paciente addPaciente(Paciente pac) {
+        return PacienteDAO.addPaciente(pac);
+    }
+    
+    // URI:
+    // /contextPath/servletPath/paciente
+    @PUT
+    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    public Paciente updatePaciente(Paciente pac) {
+        return PacienteDAO.updatePaciente(pac);
+    }
+ 
+    @DELETE
+    @Path("/{pacNo}")
+    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    public void deletePaciente(@PathParam("pacNo") String pacNo) {
+        PacienteDAO.deletePaciente(pacNo);
     }
 }
